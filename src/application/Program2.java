0e668f9ea01e788653e4abc094b8f05b112b8080
Marcos Vinicius <marcos.carini22@gmail.com>
2025-03-07 03:15:10 -0300

    feat: add department findById implementation

---
+++ src/application/Program2.java
@@ -0,0 +1,23 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.util.Scanner;
+
+public class Program2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("=== TEST 1: findById =======");
+        Department dep = departmentDao.findById(1);
+        System.out.println(dep);
+
+
+
+        sc.close();
+    }
+}
